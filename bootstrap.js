ca25c27d1c7c2c0ade84c9550cf8bc7a8dfc6889
Kelvin <kelvinmusipha@gmail.com>
2024-05-01 14:31:24 -0700

    sticky navbar

--- bootstrap.js
+++ bootstrap.js
@@ -1,3 +1,15 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+document.addEventListener('scroll', () => {
+  const header = document.querySelector('.navigation');
+
+  if(window.scrollY > 0) {
+      header.classList.add ('scrolled');
+  }
+  else {
+      header.classList.remove('scrolled');
+  }
+}) 
+  
