11af27b06731bab5cefa9e2533a15a0ce59d1326
Kelvin <kelvinmusipha@gmail.com>
2024-05-13 05:45:04 -0700

    jjgkj

--- bootstrap.js
+++ bootstrap.js
@@ -2,14 +2,14 @@ function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
 
-document.addEventListener('scroll', () => {
-  const header = document.querySelector('.navigation');
-
-  if(window.scrollY > 0) {
-      header.classList.add ('scrolled');
-  }
-  else {
-      header.classList.remove('scrolled');
-  }
-}) 
-  
+// Now we are changing the navbar background color
+function handleScroll() {
+    const header = document.querySelector('.navigation');
+    if (window.scrollY>0) {
+        header.classList.add('scrolled');
+    }
+    else {
+        header.classList.remove('scrolled');
+    }
+}
+document.addEventListener('scroll', handleScroll);
